eee0da54d80db960256d50ecdc6385f3cbb5b303
Muhammad Syahrulah <muhammadsyahrullah26@gmail.com>
2026-04-30 16:38:05 +0700

    fix : error deploy cause of use client

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { HeaderSection } from "@/components/products/HeaderSection";
 import { ServiceSection } from "@/components/products/ServiceSection";
 import { Metadata } from "next";
@@ -24,7 +23,7 @@ export const metadata: Metadata = {
 export default function ProductsPage() {
   return (
     <>
-      <section className="py-20 px-17 bg-cloud-white overflow-hidden">
+      <section className="py-20 px-20 bg-cloud-white overflow-hidden">
         <HeaderSection />
         <ServiceSection />
       </section>
